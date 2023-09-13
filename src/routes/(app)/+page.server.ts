7c712a03c63dee5491b71eeeccce0609be3f3671
Darren Paul <drobertpaul@gmail.com>
2023-09-13 18:59:39 +0200

    debugging region not working

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -3,13 +3,16 @@ import { findCountryCurrency } from "$lib/constants/shippingCountries.js";
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch, getClientAddress, cookies }) {
 	const clientAddress = getClientAddress();
+	console.log("load ~ clientAddress:", clientAddress);
 	// const clientAddress = "185.108.105.72";
 
 	const countryResponse = await fetch(`https://api.country.is/${clientAddress}`);
 	const { countryData } = await countryResponse.json();
 	const isoCode: string = countryData?.isoCode || "AT";
+	console.log("load ~ isoCode:", isoCode);
 
 	const currencyCode = findCountryCurrency(isoCode);
+	console.log("load ~ currencyCode:", currencyCode);
 	console.log("load ~ countryData:", isoCode);
 	cookies.set("currency", currencyCode);
 
