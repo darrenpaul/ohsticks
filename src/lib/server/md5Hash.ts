4eb64cc8d6a4ed6d7982af6383142d3606236a60
Darren Paul <drobertpaul@gmail.com>
2023-09-18 15:40:07 +0200

    removed md5

--- src/lib/server/md5Hash.ts
+++ src/lib/server/md5Hash.ts
@@ -1,13 +1,10 @@
-import { ORDER_SALT_KEY } from "$env/static/private";
-import { getUnixTime } from "date-fns";
-import crypto from "crypto";
+// import { ORDER_SALT_KEY } from "$env/static/private";
+// import { getUnixTime } from "date-fns";
+// import crypto from "crypto";
 
 export default async (email: string) => {
-	const timestamp = getUnixTime(new Date());
-
-	const stringToHash = `${email}&${timestamp}&${ORDER_SALT_KEY}`;
-
-	const hash = crypto.createHash("md5").update(stringToHash).digest("hex");
-
-	return hash;
+	// const timestamp = getUnixTime(new Date());
+	// const stringToHash = `${email}&${timestamp}&${ORDER_SALT_KEY}`;
+	// const hash = crypto.createHash("md5").update(stringToHash).digest("hex");
+	// return hash;
 };
