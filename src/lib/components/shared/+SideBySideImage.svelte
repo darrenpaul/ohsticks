9a3d6cf492890ff56af5f2e33d6b70a5af9e1486
Darren Paul <drobertpaul@gmail.com>
2023-09-19 12:56:41 +0200

    replaced images with supabase storage

--- src/lib/components/shared/+SideBySideImage.svelte
+++ src/lib/components/shared/+SideBySideImage.svelte
@@ -1,15 +1,14 @@
-<script lang="ts">
-</script>
-
 <div class="side-by-side-image">
 	<img
-		src="https://firebasestorage.googleapis.com/v0/b/ohsticks-37fa7.appspot.com/o/page%2Fohsticks-sticker-bag.webp?alt=media&token=8066933f-a352-467e-8e8b-2d8492bbbf16"
+		width="auto"
+		src="https://pziocbmxbpurobqznrjs.supabase.co/storage/v1/object/public/page/home/ohsticks-sticker-bag.webp"
 		alt="Preview"
 		loading="lazy"
 	/>
 
 	<img
-		src="https://firebasestorage.googleapis.com/v0/b/ohsticks-37fa7.appspot.com/o/page%2Fohsticks-sticker-on-laptop.webp?alt=media&token=0fde785a-213a-4a5a-9dca-a44aef9b055b"
+		width="auto"
+		src="https://pziocbmxbpurobqznrjs.supabase.co/storage/v1/object/public/page/home/ohsticks-sticker-on-laptop.webp"
 		alt="Preview"
 		loading="lazy"
 	/>
